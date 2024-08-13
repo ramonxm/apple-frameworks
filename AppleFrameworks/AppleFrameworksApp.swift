7ed26259ea6bc6c657c317313e7c5b8c895ec592
Ramon Xavier <ramon1234rxm@gmail.com>
2024-08-13 20:30:41 -0300

    initial commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Ramon Xavier on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
