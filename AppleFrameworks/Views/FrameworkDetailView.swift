22b18b721b249d9e60a67d259b9ba32134412243
Ramon Xavier <ramon1234rxm@gmail.com>
2024-08-19 21:19:49 -0300

    feat: create framework detail view

---
+++ AppleFrameworks/Views/FrameworkDetailView.swift
@@ -0,0 +1,55 @@
+//
+//  FrameworkDetailView.swift
+//  AppleFrameworks
+//
+//  Created by Ramon Xavier on 19/08/24.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var framework: Framework
+    
+    
+    var body: some View {
+        VStack {
+            closeButtonView
+                .padding()
+            Spacer()
+            FrameworkTitle(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            Button {
+            } label: {
+                AFButton(title: Constants.title)
+            }
+        }
+    }
+    
+    private var closeButtonView: some View {
+        HStack {
+            Spacer()
+            Button {
+            } label: {
+                Image(systemName: Constants.imageName)
+                    .foregroundStyle(Constants.foregroundColor)
+                    .imageScale(.large)
+                    .frame(width: Constants.buttonWidth, height: Constants.buttonHeight)
+            }
+        }
+    }
+    
+   private struct Constants {
+       static let title: String = "Learn More"
+       static let imageName: String = "xmark"
+       static let buttonWidth: CGFloat = 44
+       static let buttonHeight: CGFloat = 44
+       static let foregroundColor: Color = Color(.label)
+    }
+    
+}
+
+#Preview {
+    FrameworkDetailView(framework: MockData.sampleFramework)
+}
